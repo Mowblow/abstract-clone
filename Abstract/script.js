ab8410e858597623a3f66472e9254d8af6c33eee
Mowblow <110473446+Mowblow@users.noreply.github.com>
2024-12-06 03:36:26 -0800

    Delete Abstract directory

--- Abstract/script.js
+++
@@ -1,8 +0,0 @@
-const hamMenu = document.querySelector(".ham_menu");
-
-const offScreenMenu = document.querySelector(".offscreen");
-
-hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-});
